4ccbc2707b99c6bf456391681e9958e6f3046986
andao-information <535420980@qq.com>
2021-06-02 19:25:40 +0800

    0602

--- test.c
+++ test.c
@@ -6,10 +6,24 @@ struct Books
    char  author[50];
    char  subject[100];
    int   book_id;
-} ;
- 
+};
+struct 
+{
+   char  title[50];
+   char  mm[50];
+   char  subject[100];
+   int   book_id;
+}shit= {"C 语言", "RUNOOB", "编程语言",123123};
 int main()
 {
+    int list [5];
     struct Books book = {"C 语言", "RUNOOB", "编程语言", 123456};
+    struct Books * p_book;
     printf("title : %s\nauthor: %s\nsubject: %s\nbook_id: %d\n", book.title, book.author, book.subject, book.book_id);
+    printf("shit book id is %d \n",shit.book_id);
+    
+    for (int i=0;i<5;i++){
+        printf("list is %p \n",list[i]);
+    }
+    return 0;
 } 
